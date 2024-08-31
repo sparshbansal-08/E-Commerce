7651da6fcf9d0ccb9a83adf65e13f8d04454edb1
Sparsh Bansal <sparshbansal787@gmail.com>
2024-08-31 20:39:10 +0530

    hero section

--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import Hero from "../Components/Hero/Hero";
 const Shop = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
 };
 export default Shop;
