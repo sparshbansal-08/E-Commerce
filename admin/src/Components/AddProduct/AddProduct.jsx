dc3dc84232cadbbbef8adb794e418916d4522f19
Sparsh Bansal <sparshbansal787@gmail.com>
2024-11-24 20:58:37 +0530

    admin panel

---
+++ admin/src/Components/AddProduct/AddProduct.jsx
@@ -0,0 +1,99 @@
+import React, { useState } from "react";
+import "./AddProduct.css";
+import upload_area from "../../assets/upload_area.svg";
+const AddProduct = () => {
+  const [image, setImage] = useState(false);
+  const [productDetails, setProductDetails] = useState({
+    name: "",
+    image: "",
+    category: "women",
+    new_price: "",
+    old_price: "",
+  });
+
+  const imageHandler = (e) => {
+    setImage(e.target.files[0]);
+  };
+
+  const changeHandler = (e) => {
+    setProductDetails({ ...productDetails, [e.target.name]: e.target.value });
+  };
+
+  const Add_Product = async () => {
+    console.log(productDetails);
+  };
+
+  return (
+    <div className="add-product">
+      <div className="addproduct-itemfield">
+        <p>Product Title</p>
+        <input
+          value={productDetails.name}
+          onChange={changeHandler}
+          type="text"
+          name="name"
+          placeholder="Text Here"
+        />
+      </div>
+      <div className="addproduct-price">
+        <div className="addproduct-itemfield">
+          <p>Price</p>
+          <input
+            value={productDetails.old_price}
+            onChange={changeHandler}
+            type="text"
+            name="old_price"
+            placeholder="Text Here"
+          />
+        </div>
+        <div className="addproduct-itemfield">
+          <p>Offer_Price</p>
+          <input
+            value={productDetails.new_price}
+            onChange={changeHandler}
+            type="text"
+            name="new_price"
+            placeholder="Text Here"
+          />
+        </div>
+      </div>
+      <div className="addproduct-itemfield">
+        <p>Product Category</p>
+        <select
+          value={productDetails.category}
+          onChange={changeHandler}
+          name="category"
+          className="add-product-selector"
+        >
+          <option value="women">Women</option>
+          <option value="men">Men</option>
+          <option value="kid">Kid</option>
+        </select>
+      </div>
+      <div className="addproduct-itemfield">
+        <label htmlFor="file-input">
+          <img
+            src={image ? URL.createObjectURL(image) : upload_area}
+            className="addproduct-thumbnail-img"
+          />
+        </label>
+        <input
+          onChange={imageHandler}
+          type="file"
+          id="file-input"
+          name="image"
+          hidden
+        />
+      </div>
+      <button
+        onClick={() => {
+          Add_Product();
+        }}
+        className="addproduct-btn"
+      >
+        Add
+      </button>
+    </div>
+  );
+};
+export default AddProduct;
