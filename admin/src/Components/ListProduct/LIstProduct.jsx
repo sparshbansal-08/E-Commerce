67e8442b06f8efa7c46e0f3de9c39c50df621cb3
Sparsh Bansal <sparshbansal787@gmail.com>
2024-11-25 23:42:48 +0530

    admin panel update

--- admin/src/Components/ListProduct/LIstProduct.jsx
+++ admin/src/Components/ListProduct/LIstProduct.jsx
@@ -1,7 +1,58 @@
 import React from "react";
 import "./ListProduct.css";
+import { useEffect, useState } from "react";
+import cross_icon from "../../assets/cross_icon.png";
 const ListProduct = () => {
-  return <div className="list-product"></div>;
+  const [allproducts, setAllProducts] = useState([]);
+  const fetchInfo = async () => {
+    await fetch("http://localhost:4000/allproducts")
+      .then((res) => res.json())
+      .then((data) => {
+        setAllProducts(data);
+      });
+  };
+  useEffect(() => {
+    fetchInfo();
+  }, []);
+
+  return (
+    <div className="list-product">
+      <h1>All Products List</h1>
+      <div className="listproduct-format-main">
+        <p>Products</p>
+        <p>Title</p>
+        <p>Old Price</p>
+        <p>New Price</p>
+        <p>Catogory</p>
+        <p>Remove</p>
+      </div>
+      <div className="listproduct-allproducts">
+        <hr />
+        {allproducts.map((product, index) => {
+          return (
+            <div
+              key={index}
+              className="listproduct-format-main listproduct-format"
+            >
+              <img
+                src={product.image}
+                alt=""
+                className="listproduct-product-icon"
+              />
+              <p>{product.name}</p>
+              <p>${product.old_price}</p>
+              <p>${product.new_price}</p>
+              <img
+                src={cross_icon}
+                alt=""
+                className="listproduct-remove-icon"
+              />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default ListProduct;
--- admin/src/Pages/Admin/Admin.jsx
+++ admin/src/Pages/Admin/Admin.jsx
@@ -3,7 +3,7 @@ import "./Admin.css";
 import Sidebar from "../../Components/Sidebar/Sidebar";
 import { Routes, Route } from "react-router-dom";
 import AddProduct from "../../Components/AddProduct/AddProduct";
-
+import ListProduct from "../../Components/ListProduct/ListProduct";
 const Admin = () => {
   return (
     <div className="admin">
@@ -11,7 +11,7 @@ const Admin = () => {
       <div className="admin-content">
         <Routes>
           <Route path="/addproduct" element={<AddProduct />} />
-          <Route path="/listproduct" element={<AddProduct />} />
+          <Route path="/listproduct" element={<ListProduct />} />
         </Routes>
       </div>
     </div>
